9096c040b654c9225b85afbae1da1d786e40926a
Steven <steven11@mail.usf.edu>
2017-01-14 21:21:08 -0500

    Initial Upload of the Common Library.

---
+++ Common/Common/ArrayLogic/ArrayIO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ArrayLogic
+{
+    public static class ArrayIO
+    {
+        public static void PrintArray(object array, StreamWriter writer = null)
+        {
+            if (typeof(int[][]) == array.GetType())
+            {
+                //Type pointer for easy method reading/ usage
+                int[][] typedArray = (int[][])array;
+
+                for (int i = 0; i < typedArray.Length; i++)
+                {
+                    if (writer != null)
+                    {
+                        writer.WriteLine(string.Join("", typedArray[i]));
+                    }
+
+                    Console.WriteLine(string.Join("", typedArray[i]));
+                }
+
+                if (writer != null)
+                {
+                    writer.WriteLine();
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
